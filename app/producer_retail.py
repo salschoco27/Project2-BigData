f774fed741730578f4258bf7b3847518a732afdf
adlyatarisa <adlyatarisa@gmail.com>
2025-05-29 21:57:38 +0700

    feat: convert xlsx to csv

--- app/producer_retail.py
+++ app/producer_retail.py
@@ -2,9 +2,13 @@ import time
 import random
 import csv
 from kafka import KafkaProducer
+import pandas as pd
 
 producer = KafkaProducer(bootstrap_servers='kafka:9092')
 topic = 'retail-transactions'
+
+df = pd.read_excel('online_retail_II.xlsx')
+df.to_csv('retail_dataset.csv', index=False)
 
 with open('retail_dataset.csv') as file:
     reader = csv.DictReader(file)
